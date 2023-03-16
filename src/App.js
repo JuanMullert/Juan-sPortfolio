a1266fadaf73938e01b5f0035fbbff400230e2fc
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-03-16 18:07:40 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -9,45 +9,45 @@ import Contact2 from './components/Contact'
 
 function App() {
   return (
-   <>
-    <header>
-   <a href='/'> <h1 id='jdtitle'>Juan Diego Mullert</h1> </a>
-   <nav>
+    <>
+      <header>
+        <a href='/'> <h1 id='jdtitle'>Juan Diego Mullert</h1> </a>
+        <nav>
 
 
-<ul class="nav justify-content-center">
-  <li class="nav-item">
-    <a class="nav-link active" href="/Juan-sPortfolio/AboutMe">About</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="/Portfolio">Portfolio</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="/Resume">Resume</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="/Contact2">Contact</a>
-  </li>
-</ul>
+          <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link active" href="/Juan-sPortfolio/AboutMe">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Resume">Resume</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Contact2">Contact</a>
+            </li>
+          </ul>
 
         </nav>
-    
-   </header>
 
-   <div className='navlinks'>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/Juan-sPortfolio/AboutMe' element={<About/>} />
-      <Route path='/Resume' element={<Resume/>} />
-      <Route path='/Contact2' element={<Contact2/>} />
-      <Route path='/Portfolio' element={<Portfolio/>} />
-      <Route path='/' element={<Home/>} />
-      
-   </Routes>
-    </BrowserRouter>
-   </div>
+      </header>
 
-   </>
+      <div className='navlinks'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/Juan-sPortfolio/AboutMe' element={<About />} />
+            <Route path='/Resume' element={<Resume />} />
+            <Route path='/Contact2' element={<Contact2 />} />
+            <Route path='/Portfolio' element={<Portfolio />} />
+            <Route path='/' element={<Home />} />
+
+          </Routes>
+        </BrowserRouter>
+      </div>
+
+    </>
   );
 }
 
